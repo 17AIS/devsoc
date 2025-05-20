60d787733349701e5840bf5e7ba8258ef9978445
Islam Aliemul <islamaliemul@Someones-Laptop.local>
2025-05-20 19:52:47 +0900

    added quotes, gradient background, and hyperlinked interactive yinyang

--- public/front.js
+++ public/front.js
@@ -1,9 +1,31 @@
-
+const wrong = document.querySelector("#wrong")
 function stuff()  {
-        window.alert("hello");
-    
-}
+  let choice = prompt("yin or yang?")
+  if (choice.toLocaleLowerCase() === "yin") {
+    document.getElementById("body").style.background = "white";
+    document.getElementById("body").style.color = "black";
+    let image = document.getElementById("yingyang")
+    image.src = "yinyang.png"
+    wrong.innerText = ""
 
+  } else if (choice.toLocaleLowerCase() === "yang") {
+    document.getElementById("body").style.background = "black";
+    document.getElementById("body").style.color = "white";
+    let image = document.getElementById("yingyang")
+    image.src = "yinyang.png"
+    wrong.innerText = ""
+    
+  } else {
+    document.getElementById("body").style.background = "rgb(255, 165, 158)";
+    document.getElementById("body").style.color = "rgb(72, 41, 41)";
+    let image = document.getElementById("yingyang")
+    image.src = "sadness.png"
+    wrong.innerText = "You Chose Wrong"
+
+
+  }
+  console.log("it works")
+}
 const windowButton = document.querySelector("#windowButton");
 const log1 = document.querySelector("#log");
 
