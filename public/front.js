41602cd5d59bcc00df2d3a2115c6222573a1b4d2
AIS (REAL) <potato@jeffbezos.com>
2025-05-27 14:32:39 +0900

    added content to pages

--- public/front.js
+++ public/front.js
@@ -26,30 +26,3 @@ function stuff()  {
   }
   console.log("it works")
 }
-const windowButton = document.querySelector("#windowButton");
-const log1 = document.querySelector("#log");
-
-windowButton.addEventListener("click", () => {
-  if (window.confirm("Do you want to open in new tab?")) {
-    window.open("https://www.google.com/");
-  } else {
-    log1.innerText = "Glad you're staying!";
-  }
-});
-
-const signButton = document.querySelector("#signButton");
-const log = document.querySelector("#log");
-
-signButton.addEventListener("click", () => {
-  let sign = prompt("What's your sign?");
-
-  if (sign === null) {
-    log.innerText = "OK, maybe next time.";
-  } else if (sign.toLowerCase() === "") {
-    log.innerText = "Don't be shy, enter your sign!";
-  } else if (sign.toLowerCase() === "scorpio") {
-    log.innerText = "Wow! I'm a Scorpio too!";
-  } else {
-    log.innerText = `${sign} is my favorite!`;
-  }
-});
